19c5402d00ea19e9c54a5b689224fdb42880e394
dooouglas <dougllas2812@gmail.com>
2025-02-03 21:28:05 -0300

    Projeto Click & Color - commit 1

---
+++ index.js
@@ -0,0 +1,13 @@
+const cores = ['Green', 'Yellow', 'Black', 'Blue', 'Purple', 'Pink', 'Red', 'Brown', 'Violet', 'White', 'Gray', 'Orange', 'Gold', 'Magenta', 'Salmon', 'Turquoise']
+const btn = document.getElementById('btn')
+const cor = document.querySelector('.color')
+
+btn.addEventListener('click', function() {
+    const randomColor = getRandomColor()
+    document.body.style.background = cores[randomColor]    
+    cor.textContent = cores[randomColor]
+})
+
+function getRandomColor () {
+    return Math.floor(Math.random() * cores.length)
+}
